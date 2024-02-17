c1a9071470666ae3f883a676c29131c63a1d98be
Daniel Gallardo Martos <dangalmar@alum.us.es>
2024-02-17 12:11:50 +0100

    Creacion main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+def calculate_hash():
+    # Función para calcular hash de binario/imagen/directorio
+    return None
+
+
+def check_integrity():
+    # Comprueba que el hash de un fichero coincide con el guardado
+    return None
+
+
+def begin_exam():
+    # Verificar integridad de ficheros marcados
+    return None
+
+
+def send_warning_message():
+    # Mandar un correo al cliente de que hay un problema de integridad
+    return None
+
+
+def application_screen():
+    # Pantalla de la aplicación
+    return None
+
+
+if __name__ == '__main__':
+    print("On your tutorials")
